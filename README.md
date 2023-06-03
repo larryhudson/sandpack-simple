91343e9ec60da68669a3e1e0a4bcb5bdd35a6fe0
Larry Hudson <larryhudson@hey.com>
2023-06-03 15:26:09 +1000

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Play with HTML, CSS and JavaScript
+
+Sandpack is a new tool from CodeSandbox that lets you add an interactive code editor, with a build step, to a React app. The build step happens in the browser!
+
+This is a simple web app that lets you play with HTML, CSS and JavaScript with instant feedback.
+
+I'd like to explore how this can be used to make interactive code challenges for beginner web devs.
+
+You can view the demo here: (insert demo link here). 
+
+To get up and running:
+- `git clone` this repo
+- `npm install`
+- `npm run dev`
+- open [http://localhost:5173](http://localhost:5173) in your web browser.
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+import {SandpackProvider, SandpackLayout, SandpackFileExplorer, SandpackCodeEditor, SandpackPreview, SandpackConsole} from "@codesandbox/sandpack-react";
+
+function App() {
+  const files = {
+    'script.js': '// Add some JavaScript code here',
+    'styles.css': '/* Add some CSS code here */',
+    'index.html': `<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Parcel Sandbox</title>
+  <meta charset="UTF-8" />
+  <link rel="stylesheet" href="/styles.css" />
+  <script src="/script.js"></script>
+</head>
+
+<body>
+  <h1>Hello world</h1>
+</body>
+
+</html>`}
+  return (
+    <>
+    <h1>Play with HTML, CSS and JavaScript</h1>
+
+    <SandpackProvider
+    files={files} 
+    theme="light" 
+    template="static"
+    >
+    <SandpackLayout>
+      <SandpackFileExplorer />
+      <SandpackCodeEditor closableTabs showTabs  />
+      <SandpackPreview />
+    </SandpackLayout>
+    </SandpackProvider>
+    </>
+  )
+}
+
+export default App
